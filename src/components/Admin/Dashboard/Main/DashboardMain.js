a135d7faeba35d146763855c99f33b6cd90fc818
hoainho <nhoxt@DESKTOP-HSIS64O>
2021-09-24 16:56:54 +0700

    Survey dashboard

--- src/components/Admin/Dashboard/Main/DashboardMain.js
+++ src/components/Admin/Dashboard/Main/DashboardMain.js
@@ -25,11 +25,11 @@ export default function DashboardMain() {
                 setTotalIncome(total)
             }
         })
-        requestAPI('/survey/status/1', 'GET').then(res => {
-            setCountOrderStorage(res.data)
+        requestAPI('/survey/status', 'POST', { statusId: 1 }).then(res => {
+            setCountOrderStorage(res.data?.total)
         })
-        requestAPI('/survey/status/3', 'GET').then(res => {
-            setCountOrderShipped(res.data)
+        requestAPI('/survey/status', 'POST', { statusId: 3 }).then(res => {
+            setCountOrderShipped(res.data?.total)
         })
         requestAPI('/survey/totalprice', 'GET').then(res => {
             setTopOrderSales(res.data)
