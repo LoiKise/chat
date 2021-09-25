af486fb9c230f1f572a93774fe576994066e89d2
hoainho <nhoxt@DESKTOP-HSIS64O>
2021-09-25 18:43:15 +0700

    fix product

--- src/components/Admin/Dashboard/Order/DashboardOrderEdit.js
+++ src/components/Admin/Dashboard/Order/DashboardOrderEdit.js
@@ -524,7 +524,7 @@ export default function DashboardOrderCreate(props) {
                         editable={{
                             onRowAdd: (newRow) => new Promise((resolve, reject) => {
                                 console.log(newRow);
-                                if (newRow.quantity && newRow.name && newRow.unit) {
+                                if (newRow.quantity && newRow.name && newRow.unit_id) {
                                     if (isNaN(newRow.quantity)) {
                                         enqueueSnackbar('Số lượng vui lòng nhập số', {
                                             persist: false,
@@ -562,7 +562,7 @@ export default function DashboardOrderCreate(props) {
                                 }, 1000)
                             }),
                             onRowUpdate: (updatedRow, oldRow) => new Promise((resolve, reject) => {
-                                if (updatedRow.quantity && updatedRow.name && updatedRow.unit) {
+                                if (updatedRow.quantity && updatedRow.name && updatedRow.unit_id) {
                                     if (isNaN(updatedRow.quantity)) {
                                         enqueueSnackbar('Số lượng vui lòng nhập số', {
                                             persist: false,
