f65373011699723c211db599b7f384557090abe8
Tuyen <9a5.tuyen@gmail.com>
2021-09-26 22:27:57 +0700

    Wrong if account or password

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -45,10 +45,6 @@ export default function Index() {
         autoClose: 3000,
       });
     } catch (error) {
-      toast.error("Đăng nhập thất bại", {
-        position: "top-center",
-        autoClose: 3000,
-      });
       if (error.status === 422) {
         for (const key in error.data) {
           setError(key, {
