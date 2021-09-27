e7138419edab25c3337550f1b1607055eb236fac
Tuyen <9a5.tuyen@gmail.com>
2021-09-27 22:29:12 +0700

    Fix Validate

--- src/helpers/rules.js
+++ src/helpers/rules.js
@@ -84,12 +84,12 @@ export const rules = {
       message: "Trọng lượng là bắt buộc nhập",
     },
     maxLength: {
-      value: 2,
+      value: 3,
       message: "Trọng lượng tối đa 100KG",
     },
     validate: {
       number: (value) =>
-        /^[0-9]*$/.test(value) || "Trọng lượng tối đa 100 ký tự",
+        /^[0-9]*$/.test(value) || "Vui lòng nhập đúng định dạng là số",
     },
   },
   password: {
