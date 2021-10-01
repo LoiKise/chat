cd97b907a8d60ca2c60a3e8348a8fa04b70b2b11
LoiKise <loitan.nguyennguyen@gmail.com>
2021-10-01 22:29:29 +0700

    fix navbar

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,85 +1,82 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap/dist/css/bootstrap.css';
 import EventNoteIcon from "@material-ui/icons/EventNote";
 import MonetizationOnRoundedIcon from "@material-ui/icons/MonetizationOnRounded";
 import SearchSharpIcon from "@material-ui/icons/SearchSharp";
 import { Tab, Tabs, Table } from "react-bootstrap";
-import moment from "moment";
-import CustomNoRowsOverlay from "../Admin/Dashboard/Order/CustomNoRowsOverlay";
+import moment from 'moment'
+import CustomNoRowsOverlay from "../Admin/Dashboard/Order/CustomNoRowsOverlay"
 
 import requestAPI from "../../apis";
 
 export default function Search(props) {
+
+
   const [show, setShow] = useState(false);
 
-  //search lấy giá trị
-  const [type, setType] = useState({
-    //send
-    name: "",
-    customerPhone: "",
-    date: "",
-  });
+
+  //search lấy giá trị 
+  const [type, setType] = useState({ //send
+    name: '',
+    customerPhone: '',
+    date: ''
+  })
   // const [dataFormat, setDataFormat] = useState({ //show
   //   name: '',
   //   customerPhone: '',
   //   date: '',
   // })
 
+
   const handleChangeInput = (event) => {
     let { value, name, valueAsDate } = event.target;
     // console.log({ date: event.target });
+    // let state = event.target.type === 'date' ? valueAsDate : value
     // console.log(type)
-    console.log(valueAsDate);
+    // console.log(valueAsDate)
     setType({
       ...type,
-      [name]: value,
-    });
+      [name]: value
+    })
     // setDataFormat({
     //   ...dataFormat,
     //   [name]: value
     // })
-  };
-  const [state, setstate] = useState();
+  }
+  const [state, setstate] = useState()
+
   const handleSubmit = (event) => {
     // chặn sự kiện submit browser
     event.preventDefault();
     setTimeout(() => {
       if (type.name && type.name.length > 0) {
-        setShow(true);
+        setShow(true)
         requestAPI("/search/order", "POST", type)
-          .then((res) => {
+          .then(res => {
             if (res) {
               // console.log(res.data);
               setstate(res.data);
               // setLoading(true);
             }
-          })
-          .catch((err) => console.log(err));
+          }).catch(err => console.log(err))
       } else {
         // handleShow()
+      
       }
     }, 1000);
-  };
+  }
   return (
     <div className="search">
       <div className="container">
         <div className="wrap-search">
           <Tabs defaultActiveKey="first">
-            <Tab
-              eventKey="first"
-              title={
-                <span>
-                  Vận đơn <EventNoteIcon />
-                </span>
-              }
-            >
+            <Tab eventKey="first" title={<span>Vận đơn <EventNoteIcon /></span>} >
               <div className="content__tab1">
                 <form onSubmit={handleSubmit}>
-                  <div className="form-group">
+                  <div className="form-group" >
                     <p>Tên khách hàng</p>
-                    <input
-                      type="text"
+                    <input type="text"
                       name="name"
                       className="tabs__search form-control"
                       placeholder="Nhập tên khách hàng"
@@ -87,11 +84,11 @@ export default function Search(props) {
                       value={type.name}
                       required
                     />
+                   
                   </div>
-                  <div className="form-group">
+                  <div className="form-group" >
                     <p>Số điện thoại</p>
-                    <input
-                      type="text"
+                    <input type="text"
                       name="customerPhone"
                       className="tabs__search form-control"
                       placeholder="Nhập số điện thoại"
@@ -100,95 +97,67 @@ export default function Search(props) {
                       required
                     />
                   </div>
-                  <div className="form-group">
+                  <div className="form-group" >
                     <p>Nhập ngày </p>
                     <input
                       type="date"
                       name="date"
                       className="tabs__search form-control"
                       placeholder="Nhập ngày cần tìm kiếm"
-                      min="2000-01-01"
-                      max="2030-12-31"
+                      min="2000-01-01" max="2030-12-31"
                       onChange={handleChangeInput}
                       value={type.date}
                       requiredw
                     />
                   </div>
                   <div className="form-group">
-                    <button type="submit" className="btn_tab_search">
+                    <button type="submit" className="btn_tab_search" >
                       <SearchSharpIcon /> Tra cứu vận đơn
                     </button>
                   </div>
                 </form>
               </div>
-              {show && (
-                <Table striped bordered hover size="lg">
-                  {state && state.length > 0 ? (
-                    <thead>
-                      <tr>
-                        <th className="font-weight-bold text-center">
-                          Người gửi
-                        </th>
-                        <th className="font-weight-bold text-center">
-                          Số điện thoại
-                        </th>
-                        <th className="font-weight-bold text-center">
-                          Ngày gửi
-                        </th>
-                        <th className="font-weight-bold text-center">
-                          Tên người nhận
-                        </th>
-                        <th className="font-weight-bold text-center">
-                          Hàng hóa
-                        </th>
-                        <th className="font-weight-bold text-center">
-                          Chi phí
-                        </th>
-                      </tr>
-                    </thead>
-                  ) : (
-                    <thead>
-                      <tr>
-                        <th className="font-weight-bold text-center">
-                          Kết Quả
-                        </th>
-                      </tr>
-                    </thead>
-                  )}
+              {show &&
+                <Table striped bordered hover size="lg" >
+                  {
+                    state && state.length > 0 ?
+                      <thead>
+                        <tr>
+                          <th className="font-weight-bold text-center">Người gửi</th>
+                          <th className="font-weight-bold text-center">Số điện thoại</th>
+                          <th className="font-weight-bold text-center">Ngày gửi</th>
+                          <th className="font-weight-bold text-center">Tên người nhận</th>
+                          <th className="font-weight-bold text-center">Hàng hóa</th>
+                          <th className="font-weight-bold text-center">Chi phí</th>
+                        </tr>
+                      </thead>
+                      : <thead>
+                        <tr>
+                          <th className="font-weight-bold text-center">Kết Quả</th>
+                        </tr>
+                      </thead>
+                  }
 
-                  {state && state.length > 0 ? (
-                    state?.map((item) => {
+                  {state && state.length > 0 ?
+                    state?.map(item => {
                       return (
                         <tbody>
                           <td className="h5">{item?.customerName}</td>
                           <td className="h5">{item?.customerPhone}</td>
-                          <td className="h5">
-                            {moment(item?.updatedAt).format("DD-MM-YYYY")}
-                          </td>
+                          <td className="h5">{moment(item?.updatedAt).format("DD-MM-YYYY")}</td>
                           <td className="h5">{item?.receiverName}</td>
                           <td className="h5">{item?.orderName}</td>
                           <td className="h5">{item?.totalPrice}</td>
                         </tbody>
-                      );
+                      )
                     })
-                  ) : (
-                    <tbody>
-                      <div>
-                        <CustomNoRowsOverlay />
-                      </div>
-                    </tbody>
-                  )}
+                    :
+                    <tbody><div><CustomNoRowsOverlay /></div></tbody>
+                  }
                 </Table>
-              )}
-            </Tab>
-            <Tab
-              eventKey="second"
-              title={
-                <span>
-                  Cước vận chuyển <MonetizationOnRoundedIcon />
-                </span>
               }
-            >
+            </Tab>
+            <Tab eventKey="second" title={<span>Cước vận chuyển <MonetizationOnRoundedIcon /></span>}>
               <div className="tab_content_move">
                 <p className="text-left pl-5">Gửi từ</p>
                 <div className="row">
@@ -322,15 +291,7 @@ export default function Search(props) {
                 </button>
               </div>
             </Tab>
-            <Tab
-              eventKey="third"
-              title={
-                <span>
-                  Bưu cục
-                  <EventNoteIcon />
-                </span>
-              }
-            >
+            <Tab eventKey="third" title={<span>Bưu cục<EventNoteIcon /></span>} >
               <div className="tab_content_Office row justify-content-center">
                 <div className="col-md-4 mb-4">
                   <div className="ordernow-form__userinfor-item">
@@ -372,6 +333,6 @@ export default function Search(props) {
           </Tabs>
         </div>
       </div>
-    </div>
+    </div >
   );
 }
