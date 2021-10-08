a708adb05a5523cba0620e4194404970aa956691
LoiKise <loitan.nguyennguyen@gmail.com>
2021-10-09 00:09:18 +0700

    test1

---
+++ src/components/Chatbox/Chatbox.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+
+
+
+export default function Chatbox() {
+    return (
+        <div className="message">
+            <MessengerCustomerChat 
+                pageId="101401255565164"
+                appId="399972415032380"
+            />
+        </div>
+    )
+}
