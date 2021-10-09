ade03583bef846f167bc3d87bf41727e733fbd29
LoiKise <loitan.nguyennguyen@gmail.com>
2021-10-10 00:59:29 +0700

    zalo20

--- src/components/Home/Zalo/Zalo.js
+++ src/components/Home/Zalo/Zalo.js
@@ -1,14 +1,14 @@
-import React from 'react'
+// import React from 'react'
 
-export default function Zalo() {
-    return (
-        <div>
-            {/* <div class="zalo-chat-widget" data-oaid="579745863508352884" data-welcome-message="Rất vui khi được hỗ trợ bạn!"
-                data-autopopup="0" data-width="150" data-height="300"></div> */}
+// export default function Zalo() {
+//     return (
+//         <div>
+//             {/* <div class="zalo-chat-widget" data-oaid="579745863508352884" data-welcome-message="Rất vui khi được hỗ trợ bạn!"
+//                 data-autopopup="0" data-width="150" data-height="300"></div> */}
 
-            <div class="zalo-chat-widget" data-oaid="579745863508352884" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="150" data-height="300"></div>
+//             <div class="zalo-chat-widget" data-oaid="579745863508352884" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="150" data-height="300"></div>
 
-            <script src="https://sp.zalo.me/plugins/sdk.js"></script>
-        </div>
-    )
-}
+//             <script src="https://sp.zalo.me/plugins/sdk.js"></script>
+//         </div>
+//     )
+// }
