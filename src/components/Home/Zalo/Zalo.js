d39a466bef550dd0344112725b7e656c5952aaef
LoiKise <loitan.nguyennguyen@gmail.com>
2021-10-09 21:23:57 +0700

    zalo5

--- src/components/Home/Zalo/Zalo.js
+++ src/components/Home/Zalo/Zalo.js
@@ -3,7 +3,12 @@ import React from 'react'
 export default function Zalo() {
     return (
         <div>
-            <div class="zalo-chat-widget" data-oaid="3131074221113211681" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="15" data-width="300" data-height="500"></div>
+            {/* <div class="zalo-chat-widget" data-oaid="579745863508352884" data-welcome-message="Rất vui khi được hỗ trợ bạn!"
+                data-autopopup="0" data-width="150" data-height="300"></div> */}
+
+            <div class="zalo-chat-widget" data-oaid="579745863508352884" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="150" data-height="300"></div>
+
+            <script src="https://sp.zalo.me/plugins/sdk.js"></script>
         </div>
     )
 }
