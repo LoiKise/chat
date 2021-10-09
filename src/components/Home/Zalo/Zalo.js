4565c1e4abd8bc86acbf5d0f99f882862ea4e100
LoiKise <loitan.nguyennguyen@gmail.com>
2021-10-09 18:49:07 +0700

    zalo4

---
+++ src/components/Home/Zalo/Zalo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Zalo() {
+    return (
+        <div>
+            <div class="zalo-chat-widget" data-oaid="3131074221113211681" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="15" data-width="300" data-height="500"></div>
+        </div>
+    )
+}
