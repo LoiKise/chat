f1f39cd8bac3f8f70ce559f7366014c5059a1c66
LoiKise <loitan.nguyennguyen@gmail.com>
2021-10-09 18:10:48 +0700

    zalo1

--- src/components/Home/Chatbox/Zalo.js
+++ src/components/Home/Chatbox/Zalo.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Zalo() {
     return (
         <div>
-            <div class="zalo-chat-widget" data-oaid="579745863508352884" data-welcome-message="Xin Chào! bạn cần hỗ trợ gì ?" data-autopopup="20" data-width="300" data-height="500"></div>
+            <div class="zalo-chat-widget" data-oaid="579745863508352884" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="15" data-width="" data-height=""></div>
+
         </div>
     )
 }
